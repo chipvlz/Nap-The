45b949863fcd7b3b3c35e8ec862c7f2974b8cfbd
kientranutc <kienkienutc95@gmail.com>
2018-08-16 15:49:15 +0700

    update database

--- nap-the.sql
+++ nap-the.sql
@@ -10,10 +10,31 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-08-14 15:36:00
+Date: 2018-08-16 15:48:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `api_token`
+-- ----------------------------
+DROP TABLE IF EXISTS `api_token`;
+CREATE TABLE `api_token` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of api_token
+-- ----------------------------
+INSERT INTO `api_token` VALUES ('1', '4eaba7d4f5c7883af091c70b8025f3b6', '0', '2018-08-16 04:38:59', '2018-08-16 06:08:21');
+INSERT INTO `api_token` VALUES ('2', 'b1f9384b345681ac627277c99b67d7a0', '1', '2018-08-16 04:39:09', '2018-08-16 04:39:09');
+INSERT INTO `api_token` VALUES ('3', 'cbf46ca3e4e9bf52c7103eb9ffce034c', '1', '2018-08-16 04:39:30', '2018-08-16 04:39:30');
+INSERT INTO `api_token` VALUES ('4', '6ee2abfc4f223e888e7eb77fac0524e9', '1', '2018-08-16 04:39:41', '2018-08-16 04:39:41');
 
 -- ----------------------------
 -- Table structure for `migrations`
@@ -29,6 +50,7 @@ CREATE TABLE `migrations` (
 -- ----------------------------
 INSERT INTO `migrations` VALUES ('2014_10_12_000000_create_users_table', '1');
 INSERT INTO `migrations` VALUES ('2014_10_12_100000_create_password_resets_table', '1');
+INSERT INTO `migrations` VALUES ('2018_08_16_040111_create_api_token_token', '2');
 
 -- ----------------------------
 -- Table structure for `password_resets`
@@ -61,32 +83,13 @@ CREATE TABLE `pay_card` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of pay_card
 -- ----------------------------
-INSERT INTO `pay_card` VALUES ('1', '12142352346', '35435346464636', '10000', '10000', '0964953029', '1', '2018-08-14 03:47:42', '2018-08-14 03:47:42');
-INSERT INTO `pay_card` VALUES ('2', '12142352346', '35435346464636', '10000', '10000', '0964953029', '1', '2018-08-14 03:47:44', '2018-08-14 03:47:44');
-INSERT INTO `pay_card` VALUES ('3', '12142352346', '35435346464636', '10000', '10000', '0964953029', '1', '2018-08-14 03:47:49', '2018-08-14 03:47:49');
-INSERT INTO `pay_card` VALUES ('4', '12142352346', '35435346464636', '10000', '10000', '0964953029', '1', '2018-08-14 03:47:51', '2018-08-14 03:47:51');
-INSERT INTO `pay_card` VALUES ('5', '12142352346', '35435346464636', '50000', '10000', '0964953029', '0', '2018-08-14 03:48:01', '2018-08-14 03:48:01');
-INSERT INTO `pay_card` VALUES ('6', '12142352346', '35435346464636', '50000', '10000', '0964953028', '0', '2018-08-14 03:48:10', '2018-08-14 03:48:10');
-INSERT INTO `pay_card` VALUES ('7', '12142352346', '35435346464636', '50000', '10000', '0964953028', '0', '2018-08-14 05:50:39', '2018-08-14 05:50:39');
-INSERT INTO `pay_card` VALUES ('8', '12142352346', '35435346464636', '10000', '10000', '0964953029', '1', '2018-08-14 14:02:50', '2018-08-14 14:02:50');
-INSERT INTO `pay_card` VALUES ('9', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 14:02:52', '2018-08-14 14:02:52');
-INSERT INTO `pay_card` VALUES ('10', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 14:02:56', '2018-08-14 14:02:56');
-INSERT INTO `pay_card` VALUES ('11', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 14:02:58', '2018-08-14 14:02:58');
-INSERT INTO `pay_card` VALUES ('12', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 14:03:03', '2018-08-14 14:03:03');
-INSERT INTO `pay_card` VALUES ('13', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 14:03:06', '2018-08-14 14:03:06');
-INSERT INTO `pay_card` VALUES ('14', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 14:03:10', '2018-08-14 14:03:10');
-INSERT INTO `pay_card` VALUES ('15', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 07:03:23', '2018-08-14 07:03:23');
-INSERT INTO `pay_card` VALUES ('16', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 07:03:26', '2018-08-14 07:03:26');
-INSERT INTO `pay_card` VALUES ('17', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 07:03:32', '2018-08-14 07:03:32');
-INSERT INTO `pay_card` VALUES ('18', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 07:03:36', '2018-08-14 07:03:36');
-INSERT INTO `pay_card` VALUES ('19', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 07:03:37', '2018-08-14 07:03:37');
-INSERT INTO `pay_card` VALUES ('20', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 07:03:52', '2018-08-14 07:03:52');
-INSERT INTO `pay_card` VALUES ('21', '12142352346', '35435346464636', '10000', '10000', '0964953028', '1', '2018-08-14 07:03:54', '2018-08-14 07:03:54');
+INSERT INTO `pay_card` VALUES ('1', '12142352346', '26672273664586', '50000', '10000', '0947911326', '0', '2018-08-15 11:00:23', '2018-08-15 11:00:23');
+INSERT INTO `pay_card` VALUES ('2', '12142352346', '26672273664586', '10000', '10000', '0947911326', '1', '2018-08-15 10:25:52', '2018-08-15 10:25:52');
 
 -- ----------------------------
 -- Table structure for `phone`
@@ -103,19 +106,21 @@ CREATE TABLE `phone` (
   `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of phone
 -- ----------------------------
-INSERT INTO `phone` VALUES ('1', '0964953029', 'TRATRUOC', '100000', '100000', 'admin', '2', '2018-08-14 14:02:08', '2018-08-14 07:02:08');
-INSERT INTO `phone` VALUES ('2', '0964953028', 'TRASAU', '500000', '230000', 'admin', '1', '2018-08-14 14:03:54', '2018-08-14 07:03:54');
-INSERT INTO `phone` VALUES ('5', '0964953027', 'TRATRUOC', '400000', '0', 'admin', '0', '2018-08-14 08:32:38', '2018-08-14 08:32:38');
-INSERT INTO `phone` VALUES ('6', '0962354356', 'TRATRUOC', '300000', '0', 'admin', '0', '2018-08-14 08:32:39', '2018-08-14 08:32:39');
-INSERT INTO `phone` VALUES ('7', '01639653755', 'TRATRUOC', '200000', '0', 'admin', '0', '2018-08-14 08:32:39', '2018-08-14 08:32:39');
-INSERT INTO `phone` VALUES ('9', '0964983455', 'TRATRUOC', '600000', '0', 'admin', '0', '2018-08-14 08:34:23', '2018-08-14 08:34:23');
-INSERT INTO `phone` VALUES ('10', '0964983454', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-14 08:34:23', '2018-08-14 08:34:23');
-INSERT INTO `phone` VALUES ('11', '0964983453', 'TRATRUOC', '400000', '0', 'admin', '0', '2018-08-14 08:34:23', '2018-08-14 08:34:23');
+INSERT INTO `phone` VALUES ('1', '0947601397', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 11:18:27', '2018-08-15 04:18:27');
+INSERT INTO `phone` VALUES ('2', '0943051175', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 11:16:18', '2018-08-15 11:16:18');
+INSERT INTO `phone` VALUES ('3', '0947911326', 'TRATRUOC', '500000', '20000', 'admin', '1', '2018-08-16 12:36:33', '2018-08-16 05:36:33');
+INSERT INTO `phone` VALUES ('4', '0942166276', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 00:48:16', '2018-08-15 00:48:16');
+INSERT INTO `phone` VALUES ('5', '0943734328', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 00:48:16', '2018-08-15 00:48:16');
+INSERT INTO `phone` VALUES ('6', '0948960328', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 00:48:17', '2018-08-15 00:48:17');
+INSERT INTO `phone` VALUES ('7', '0943845128', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 00:48:17', '2018-08-15 00:48:17');
+INSERT INTO `phone` VALUES ('8', '0946754992', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 00:48:17', '2018-08-15 00:48:17');
+INSERT INTO `phone` VALUES ('9', '0943053392', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 00:48:17', '2018-08-15 00:48:17');
+INSERT INTO `phone` VALUES ('10', '0945853672', 'TRATRUOC', '500000', '0', 'admin', '0', '2018-08-15 00:48:17', '2018-08-15 00:48:17');
 
 -- ----------------------------
 -- Table structure for `users`
@@ -138,9 +143,11 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`),
   UNIQUE KEY `users_phone_unique` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('1', 'admin', '', '', 'kienkienutc95@gmail.com', '0964953029', null, '', '$2y$10$B8gSJzs0fOzs0IwqWC/AzuCflEmejiK7KT2.aVXPu9TbaJ4/8YV3e', null, 'GNL2TJSGuOPR0njwp1G652IJjY0YaOFsxTFG0i6EWr8HmIl8YdvAoSMG94Ya', null, '2018-08-13 02:38:46');
+INSERT INTO `users` VALUES ('1', 'admin', 'Trần Chung Kiên', '', 'kienkienutc95@gmail.com', '0964953029', null, '', '$2y$10$B8gSJzs0fOzs0IwqWC/AzuCflEmejiK7KT2.aVXPu9TbaJ4/8YV3e', null, 'j3gvgw5NzqHKRDm765jHaLp9WqnCgmITxBU9vu0XWzEX43tcdxSKyuLfKAQh', '2018-08-15 14:14:40', '2018-08-15 04:26:07');
+INSERT INTO `users` VALUES ('3', 'member', 'Trần Chung Kiên', 'http://nap-the.local/source/avatar04.png', 'admin@gmail.com', '0964953028', null, '', '$2y$10$VAPVL25.dlkjsJ86li0ET.6BeGXqjNfWxjdoLFYW9DLASqS6iBTKC', null, null, '2018-08-16 08:42:30', '2018-08-16 08:42:30');
+INSERT INTO `users` VALUES ('4', 'kientran', 'Trần Chung Kiên', 'http://nap-the.local/source/avatar.png', 'quangminh.vu@vnnplus.vn', '0964953027', null, '', '$2y$10$V7DvwWup4iku7YakUGkHSO.adXSmY0cM0pmAU8YQF9sAe6K8rdHTq', null, null, '2018-08-16 15:44:32', '2018-08-16 15:44:32');
